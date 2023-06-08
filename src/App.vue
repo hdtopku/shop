747d2718dfada6599a996a12cb0b8ee54669817d
hdtopku <1450948930@qq.com>
2023-06-08 14:54:12 +0800

    自动描述：2023-06-08-14:54:12

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import Page from '@/components/Page.vue'
+
+const locale = zhCN
 </script>
 
 <template>
-	<Page id="app">
-		<router-view />
-	</Page>
+	<a-config-provider :locale="locale">
+		<Page id="app">
+			<router-view />
+		</Page>
+	</a-config-provider>
 </template>
 
 <style scoped></style>
--- src/views/jet/components/ChaJian.vue
+++ src/views/jet/components/ChaJian.vue
@@ -1,51 +1,37 @@
 <template>
 	<a-card>
 		<div class="tw-text-center">
-			<a-typography-title mark>请选择需要激活的软件版本</a-typography-title>
-			<a-tabs v-model:activeKey="activeKey" centered type="card" @tab-click="tabClick" @change="changePane">
-				<a-tab-pane v-if="system === 'windows'" key="2020" tab="2017-2020版">
-					<span v-if="loading">资源加载中...</span>
-				</a-tab-pane>
-				<a-tab-pane v-if="system === 'windows'" key="2022" tab="2021-2022版">
-					<span v-if="loading">资源加载中...</span>
-				</a-tab-pane>
-				<a-tab-pane v-if="system === 'mac'" key="2022" tab="2017-2022版">
-					<span v-if="loading">资源加载中...</span>
-				</a-tab-pane>
-				<a-tab-pane v-if="system === 'linux'" key="2022" tab="2021-2022版">
-					<span v-if="loading">资源加载中...</span>
-				</a-tab-pane>
-				<a-tab-pane key="2023" tab="软件下载地址"> <span v-if="loading">资源加载中...</span> </a-tab-pane>
+			<a-tabs v-if="system === 'windows'" v-model:activeKey="activeKey" centered @change="changeActiveKey">
+				<a-tab-pane key="1" tab="2017-2020版"></a-tab-pane>
+				<a-tab-pane key="2" tab="2021-2022版"></a-tab-pane>
 			</a-tabs>
-		</div>
-
-		<div v-show="showPane2023">
-			<iframe ref="pane2023" title="" src="/iframe" class="iframe"></iframe>
-		</div>
-		<div v-show="showPane2022">
-			<iframe ref="pane2022" title="" src="/iframe" class="iframe"></iframe>
-		</div>
-		<div v-show="showPane2020">
-			<iframe ref="pane2020" title="" src="/iframe" class="iframe"></iframe>
+			<a-space size="large">
+				<a-button
+					size="large"
+					class="animate__animated animate__pulse animate__infinite"
+					type="primary"
+					@click="openLink"
+					>激活插件下载</a-button
+				>
+				<a-button size="large" @click="openDownload">软件下载地址</a-button>
+			</a-space>
+			<div v-if="system !== 'windows'">
+				<a-divider></a-divider>
+				<a-card hoverable size="small">
+					<a-typography-text mark>教程中可能用到的命令：</a-typography-text
+					><a-typography-paragraph copyable code>{{ command }}</a-typography-paragraph>
+				</a-card>
+			</div>
+			<img style="width: 100%" :src="imgSrc" />
 		</div>
 	</a-card>
 </template>
 
 <script setup lang="ts">
 import browser from 'browser-tool'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
 const browserInfo = browser()
-
-const activeKey = ref('2022')
-const loading = ref(true)
-
-const pane2023 = ref()
-const pane2022 = ref()
-const pane2020 = ref()
-const showPane2023 = ref(false)
-const showPane2022 = ref(false)
-const showPane2020 = ref(false)
 
 const getSystem = () => {
 	let system = browserInfo.system.toLowerCase()
@@ -68,60 +54,39 @@ const getSystem = () => {
 	}
 }
 const system = getSystem()
+const activeKey = ref('1')
 
-const changePane = () => {
-	if (activeKey.value === '2022') {
-		showPane2023.value = false
-		showPane2022.value = true
-		showPane2020.value = false
-	} else if (activeKey.value === '2020') {
-		showPane2022.value = false
-		showPane2023.value = false
-		showPane2020.value = true
+let link = ''
+let imgSrc = ''
+let command = ''
+if (system === 'mac') {
+	link = 'https://wwlq.lanzouf.com/iUsb90uz7a8b'
+	imgSrc = 'https://article.biliimg.com/bfs/article/3a15032f9d83d805b1efd1469f076b4f7d72da14.png'
+	command = 'chmod +x mac.sh && ./mac.sh'
+} else if (system === 'linux') {
+	link = 'https://wwlq.lanzouf.com/icM290uzgeab'
+	imgSrc = 'https://article.biliimg.com/bfs/article/254ba9d6081fefb1a110271b78a735bd31640cbc.png'
+	command = 'chmod +x scripts/install.sh && ./scripts/install.sh'
+} else if (system === 'windows') {
+	link = 'https://wwlq.lanzouf.com/ilfg70uzj8le'
+	imgSrc = 'https://article.biliimg.com/bfs/article/234814979cf7abaa2fe0a1107d047f4375ded115.png'
+	command = ''
+}
+const openLink = () => {
+	window.open(link)
+}
+const openDownload = () => {
+	window.open('https://www.yuque.com/yangleduo-o3nlq/oh36ip/upgdau?singleDoc')
+}
+const changeActiveKey = (key) => {
+	if (key === '1') {
+		link = 'https://wwlq.lanzouf.com/ilfg70uzj8le'
+		imgSrc = 'https://article.biliimg.com/bfs/article/234814979cf7abaa2fe0a1107d047f4375ded115.png'
 	} else {
-		showPane2022.value = false
-		showPane2023.value = true
-		showPane2020.value = false
+		link = 'https://wwlq.lanzouf.com/iQBFv0uzjcsf'
+		imgSrc = 'https://article.biliimg.com/bfs/article/4f3b13b77f8224eae8ac4075775d8862cd454347.png'
 	}
 }
-const emit = defineEmits(['query'])
-const tabClick = () => {
-	emit('query')
-}
-onMounted(() => {
-	window.addEventListener(
-		'message',
-		() => {
-			if (system === 'windows') {
-				pane2022.value.contentWindow.postMessage(
-					{ src: 'https://www.yuque.com/yangleduo-o3nlq/oh36ip/eumuei?singleDoc' },
-					'*'
-				)
-				pane2020.value.contentWindow.postMessage(
-					{ src: 'https://www.yuque.com/yangleduo-o3nlq/oh36ip/gzxqk1?singleDoc' },
-					'*'
-				)
-			} else if (system === 'mac') {
-				pane2022.value.contentWindow.postMessage(
-					{ src: 'https://www.yuque.com/yangleduo-o3nlq/oh36ip/uskdoh?singleDoc' },
-					'*'
-				)
-			} else if (system === 'linux') {
-				pane2022.value.contentWindow.postMessage(
-					{ src: 'https://www.yuque.com/yangleduo-o3nlq/oh36ip/zi3gyy?singleDoc' },
-					'*'
-				)
-			}
-			loading.value = false
-			pane2023.value.contentWindow.postMessage(
-				{ src: 'https://www.yuque.com/yangleduo-o3nlq/oh36ip/upgdau?singleDoc' },
-				'*'
-			)
-			changePane()
-		},
-		false
-	)
-})
 </script>
 
 <style lang="less" scoped>
@@ -132,5 +97,8 @@ onMounted(() => {
 	border: 0;
 	top: 130px;
 	bottom: 100px;
+}
+img {
+	pointer-events: none;
 }
 </style>
